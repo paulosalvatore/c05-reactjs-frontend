dfad3336b224e70faaab05e00ba522bf337cc61b
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-04 20:22:53 -0300

    feat(product): create page for creating products

--- src/App.js
+++ src/App.js
@@ -6,15 +6,18 @@ import { Home } from "./pages/Home/Home";
 
 // CSS
 import "./App.css";
+import CreateProduct from "./pages/CreateProduct/CreateProduct";
 
 export function App() {
     return (
         <div className="App">
-            <header className="App-header"> Product List </header>
+            <header className="App-header">Product App</header>
 
             <div className="content">
                 <Switch>
                     <Route path="/" exact={true} component={Home} />
+
+                    <Route path="/product/create" component={CreateProduct} />
                 </Switch>
             </div>
         </div>
---
+++ src/pages/CreateProduct/CreateProduct.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CreateProduct() {
+    return <div>CreateProduct</div>;
+}
