0a9943447a2f6f0423cb9f866cd8a872c3057804
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-04 21:31:19 -0300

    feat(product): implement form (WIP)

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,13 +1,25 @@
 export const Api = {
     baseUrl: "http://localhost:3000",
 
-    // Endpoint
+    // Endpoint - Product
 
     readAllUrl: () => Api.baseUrl + "/product",
+
+    createProductUrl: () => Api.baseUrl + "/product",
 
     // GET
     buildApiGetRequest: url =>
         fetch(url, {
             method: "GET",
+        }),
+
+    // POST
+    buildApiPostRequest: (url, body) =>
+        fetch(url, {
+            method: "POST",
+            headers: new Headers({
+                "Content-type": "application/json",
+            }),
+            body: JSON.stringify(body),
         }),
 };
--- src/pages/CreateProduct/CreateProduct.js
+++ src/pages/CreateProduct/CreateProduct.js
@@ -1,5 +1,55 @@
 import React from "react";
+import { Api } from "../../api/Api";
 
 export default function CreateProduct() {
-    return <div>CreateProduct</div>;
+    const handleSubmit = async event => {
+        // Previne o comportamento padrão do submit, que no caso do form é o refresh
+        event.preventDefault();
+
+        // Obtém os dados dos inputs
+        const name = event.target.name.value;
+        const price = +event.target.price.value;
+        const imageUrl = event.target.imageUrl.value;
+
+        // Constrói um payload com esses dados
+        const payload = {
+            name,
+            price,
+            imageUrl,
+        };
+
+        // Faz uma requisição no backend
+        const response = await Api.buildApiPostRequest(
+            Api.createProductUrl(),
+            payload
+        );
+
+        console.log({ response });
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="name">Name:</label>
+                <br />
+
+                <input type="text" id="name" name="name" />
+                <br />
+
+                <label htmlFor="price">Price:</label>
+                <br />
+
+                <input type="number" id="price" name="price" />
+                <br />
+
+                <label htmlFor="imageUrl">Image URL:</label>
+                <br />
+
+                <input type="text" id="imageUrl" name="imageUrl" />
+                <br />
+
+                <input type="submit" value="Add" />
+            </form>
+        </div>
+    );
 }
