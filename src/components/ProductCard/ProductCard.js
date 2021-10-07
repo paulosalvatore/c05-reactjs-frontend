bd1a2f9febdf06d82c38330a36869a3d832337c0
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-06 22:14:30 -0300

    feat(product-card): add click to card

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 export default function ProductCard({ product }) {
+    const history = useHistory();
+
+    const handleClick = () => {
+        history.push(`/product/view/${product.id}`);
+    };
+
     return (
-        <div className="card">
+        <div className="card" onClick={() => handleClick()}>
             <div className="card__title">
                 <h1>{product.name}</h1>
             </div>
@@ -10,8 +17,8 @@ export default function ProductCard({ product }) {
             <div className="card__price">${product.price.toFixed(2)}</div>
 
             <div className="card__images">
-                {product.images.map(image => (
-                    <div className="card__image">
+                {product.images.map((image, index) => (
+                    <div key={`product_image_${index}`} className="card__image">
                         <img src={image.url} alt={product.name + "'s image"} />
                     </div>
                 ))}
