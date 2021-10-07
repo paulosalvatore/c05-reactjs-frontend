de417c57718644b7415b869ed5bd1994952ff9ff
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-06 23:03:18 -0300

    feat(product): implement update

--- src/api/Api.js
+++ src/api/Api.js
@@ -9,6 +9,8 @@ export const Api = {
 
     createProductUrl: () => Api.baseUrl + "/product",
 
+    updateUrl: id => Api.baseUrl + "/product/" + id,
+
     // GET
     buildApiGetRequest: url =>
         fetch(url, {
@@ -19,6 +21,16 @@ export const Api = {
     buildApiPostRequest: (url, body) =>
         fetch(url, {
             method: "POST",
+            headers: new Headers({
+                "Content-type": "application/json",
+            }),
+            body: JSON.stringify(body),
+        }),
+
+    // PATCH
+    buildApiPatchRequest: (url, body) =>
+        fetch(url, {
+            method: "PATCH",
             headers: new Headers({
                 "Content-type": "application/json",
             }),
--- src/pages/UpdateProduct/UpdateProduct.js
+++ src/pages/UpdateProduct/UpdateProduct.js
@@ -1,5 +1,125 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Api } from "../../api/Api";
 
-export default function UpdateProduct() {
-    return <div>UpdateProduct</div>;
+export default function UpdateProduct(props) {
+    const id = props.match.params.id;
+
+    const [product, setProduct] = useState(undefined);
+
+    useEffect(() => {
+        const loadProduct = async () => {
+            const response = await Api.buildApiGetRequest(Api.readByIdUrl(id));
+
+            const results = await response.json();
+
+            setProduct(results);
+        };
+
+        loadProduct();
+    }, [id]);
+
+    if (!product) {
+        return <div>Loading...</div>;
+    }
+
+    const handleSubmit = async event => {
+        // Previne o comportamento padrão do submit, que no caso do form é o refresh
+        event.preventDefault();
+
+        // Obtém os dados dos inputs
+        const name = event.target.name.value;
+        const price = +event.target.price.value;
+        const imageUrl = event.target.imageUrl.value;
+
+        // Constrói um payload com esses dados
+        const payload = {
+            name,
+            price,
+            images: [
+                {
+                    url: imageUrl,
+                },
+            ],
+        };
+
+        // Faz uma requisição no backend
+        const response = await Api.buildApiPatchRequest(
+            Api.updateUrl(id),
+            payload
+        );
+
+        const body = await response.json();
+
+        if (response.status === 200) {
+            // Product updated successfully
+
+            const id = body.id;
+
+            props.history.push(`/product/view/${id}`);
+        } else {
+            // Error
+        }
+    };
+
+    return (
+        <div>
+            <form className="form" onSubmit={handleSubmit}>
+                <div>
+                    <label className="form__label" htmlFor="name">
+                        Name:
+                    </label>
+                </div>
+
+                <div>
+                    <input
+                        className="form__input-text"
+                        type="text"
+                        id="name"
+                        name="name"
+                        defaultValue={product.name}
+                    />
+                </div>
+
+                <div>
+                    <label className="form__label" htmlFor="price">
+                        Price:
+                    </label>
+                </div>
+
+                <div>
+                    <input
+                        className="form__input-text"
+                        type="number"
+                        id="price"
+                        name="price"
+                        defaultValue={product.price}
+                    />
+                </div>
+
+                <div>
+                    <label className="form__label" htmlFor="imageUrl">
+                        Image URL:
+                    </label>
+                </div>
+
+                <div>
+                    <input
+                        className="form__input-text"
+                        type="text"
+                        id="imageUrl"
+                        name="imageUrl"
+                        defaultValue={product.images[0]?.url}
+                    />
+                </div>
+
+                <div>
+                    <input
+                        className="form__submit button button--primary"
+                        type="submit"
+                        value="Edit"
+                    />
+                </div>
+            </form>
+        </div>
+    );
 }
