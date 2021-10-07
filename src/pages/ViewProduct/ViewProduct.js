352764b573cb705b63603313b403bd7dc3ae51ab
Paulo Salvatore <salvatoregames@gmail.com>
2021-10-06 22:43:30 -0300

    feat(product): add link button and add style to buttons

--- src/pages/ViewProduct/ViewProduct.js
+++ src/pages/ViewProduct/ViewProduct.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Api } from "../../api/Api";
+import LinkButton from "../../components/LinkButton/LinkButton";
 import ProductCard from "../../components/ProductCard/ProductCard";
+
+import "./ViewProduct.css";
 
 export default function ViewProduct(props) {
     const id = props.match.params.id;
@@ -23,5 +26,25 @@ export default function ViewProduct(props) {
         return <div>Loading...</div>;
     }
 
-    return <ProductCard product={product} />;
+    return (
+        <div className="product">
+            <div className="product__buttons">
+                <LinkButton
+                    to={"/product/edit/" + id}
+                    className="button button--primary"
+                >
+                    Edit
+                </LinkButton>
+
+                <LinkButton
+                    to={"/product/delete/" + id}
+                    className="button button--danger"
+                >
+                    Delete
+                </LinkButton>
+            </div>
+
+            <ProductCard product={product} />
+        </div>
+    );
 }
